74dd1c3988b07de539297d98aafeb17323ee3b14
dcnis <bgk.dennis@yahoo.de>
2020-02-28 22:18:07 +0100

    added conditional welcome message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h1>
-      <span class="level">Welcome </span> {{userdata && userdata.name}}
+      <div v-if="isLoggedIn" ><span class="level">Welcome </span> {{userdata.name}}</div>
+      <div v-else ><span class="level">Welcome </span> to Popup Chinese</div>
     </h1>
     <p>Congratulations on finding the world's best community for learning Chinese. If you're just getting started, check out our collection of Chinese podcasts first. These are broken down by difficulty and can be covered in any order: important words and concepts will repeat. Once you understand the basics move on to our collection of Chinese videos, or test yourself with our collection of sample hsk tests. More advanced students are encouraged to check out our archive of manually annotated Chinese short stories.</p>
 
