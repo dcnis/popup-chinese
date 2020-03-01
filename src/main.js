27b54879a50e7681b6b0a0b4e76ddb563175989f
dcnis <bgk.dennis@yahoo.de>
2020-03-01 11:14:56 +0100

    removed apollo from project

--- src/main.js
+++ src/main.js
@@ -3,20 +3,12 @@ import vuetify from './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store/store';
-import apollo from './apolloClient';
-import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false;
-Vue.use(VueApollo);
-
-const apolloProvider = new VueApollo({
-  defaultClient: apollo
-});
 
 new Vue({
   router,
   store,
   vuetify,
-  apolloProvider,
   render: h => h(App)
 }).$mount('#app');
