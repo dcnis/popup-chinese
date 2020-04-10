97ac9e9ce44961dd23cd5609e7dd385a485db83b
dcnis <bgk.dennis@yahoo.de>
2020-04-10 17:37:41 +0200

    refactoring

--- src/components/PodcastPlayer.vue
+++ src/components/PodcastPlayer.vue
@@ -35,7 +35,6 @@
 </template>
 
 <script>
-const formatTime = second => new Date(second * 1000).toISOString().substr(11, 8);
 const timeOffset = 100000;
 
 export default {
@@ -58,40 +57,17 @@ export default {
       default: null
     }
   },
-  computed: {
-    duration() {
-      return this.audio ? formatTime(this.totalDuration) : '';
-    },
-    pWidth() {
-    //   if(this.slider){
-    //     return this.slider.offsetWidth * this.scale + 'px';
-    //   }else{
-    //     return 0 + 'px'
-    //   }
-      return 50 + 'px';
-    },
-    left() {
-      return 20 + 'px';
-    }
-  },
   data() {
     return {
-      test: 140,
       firstPlay: true,
-      isMuted: false,
       loading: true,
       playing: false,
       paused: false,
-      percentage: 20,
-      currentTime: '00:00:00',
       audio: undefined,
-      totalDuration: 0,
-      currentValue: 0,
       sliderValue: 0,
       trackDuration: 0
     };
   },
-
   methods: {
     startFromBeginning() {
       this.audio.currentTime = 0;
@@ -101,14 +77,6 @@ export default {
     },
     skipSeconds(seconds) {
       this.audio.currentTime = (this.audio.currentTime * timeOffset + (seconds * timeOffset)) / timeOffset;
-    },
-    setPosition() {
-      this.audio.currentTime = parseInt(this.audio.duration / 100 * this.percentage);
-    },
-    stop() {
-      this.paused = this.playing = false;
-      this.audio.pause();
-      this.audio.currentTime = 0;
     },
     playAudio() {
       this.firstPlay = false;
@@ -125,65 +93,10 @@ export default {
       this.playing = false;
       this.audio.pause();
     },
-    download() {
-      this.audio.pause();
-      window.open(this.file, 'download');
-    },
     mute() {
       this.isMuted = !this.isMuted;
       this.audio.muted = this.isMuted;
       this.volumeValue = this.isMuted ? 0 : 75;
-    },
-    reload() {
-      this.audio.load();
-    },
-    _handleLoaded: function() {
-      if (this.audio.readyState >= 2) {
-        if (this.audio.duration === Infinity) {
-          // Fix duration for streamed audio source or blob based
-          // https://stackoverflow.com/questions/38443084/how-can-i-add-predefined-length-to-audio-recorded-from-mediarecorder-in-chrome/39971175#39971175
-          this.audio.currentTime = 1e101;
-          this.audio.ontimeupdate = () => {
-            this.audio.ontimeupdate = () => {};
-            this.audio.currentTime = 0;
-            this.totalDuration = parseInt(this.audio.duration);
-            this.loaded = true;
-          };
-        } else {
-          this.totalDuration = parseInt(this.audio.duration);
-          this.loaded = true;
-        }
-
-        if (this.autoPlay) this.audio.play();
-      } else {
-        throw new Error('Failed to load sound file');
-      }
-    },
-    _handlePlayingUI: function(e) {
-      this.percentage = this.audio.currentTime / this.audio.duration * 100;
-      this.currentTime = formatTime(this.audio.currentTime);
-    },
-    _handlePlayPause: function(e) {
-      if (e.type === 'play' && this.firstPlay) {
-        // in some situations, audio.currentTime is the end one on chrome
-        this.audio.currentTime = 0;
-        if (this.firstPlay) {
-          this.firstPlay = false;
-        }
-      }
-      if (e.type === 'pause' && this.paused === false && this.playing === false) {
-        this.currentTime = '00:00:00';
-      }
-    },
-    _handleEnded() {
-      this.paused = this.playing = false;
-    },
-    init: function() {
-      this.audio.addEventListener('timeupdate', this._handlePlayingUI);
-      this.audio.addEventListener('loadeddata', this._handleLoaded);
-      this.audio.addEventListener('pause', this._handlePlayPause);
-      this.audio.addEventListener('play', this._handlePlayPause);
-      this.audio.addEventListener('ended', this._handleEnded);
     },
     loadData() {
       this.trackDuration = this.audio.duration * timeOffset;
@@ -199,14 +112,14 @@ export default {
         this.playing = false;
       }
     },
-    myInit() {
+    init() {
       this.audio.addEventListener('loadeddata', this.loadData);
       this.audio.addEventListener('timeupdate', this.updateSlider);
     }
   },
   mounted() {
     this.audio = new Audio('http://popupchinese.com/data/26/audio.mp3');
-    this.myInit();
+    this.init();
   },
   filters: {
     formatTime(s) {
@@ -242,49 +155,6 @@ export default {
     margin: 20px auto;
 }
 
-.vueAudioBetter .slider {
-  position: relative;
-  margin: 26px auto;
-  width: 80%;
-  height: 10px;
-  background: rgb(248, 247, 247);
-  border-radius: 5px;
-  cursor: pointer;
-}
-
-.slidecontainer {
-    width: 100%;
-}
-
-.slider {
-    -webkit-appearance: none;
-    margin: 20px auto;
-    width: 90%;
-    height: 10px;
-    background: white;
-    outline: none;
-    border-radius: 5px;
-}
-
-.slider::-webkit-slider-thumb {
--webkit-appearance: none;
-appearance: none;
-width: 20px;
-height: 20px;
-border-radius: 50%;
-background: #4CAF50;
-cursor: pointer;
-opacity: 80%;
-}
-
-.slider::-moz-range-thumb {
-width: 25px;
-height: 25px;
-border-radius: 50%;
-background: #4CAF50;
-cursor: pointer;
-}
-
 .playerButtons {
 position: relative;
 margin: 0 auto;
@@ -311,31 +181,8 @@ text-align: center;
     transform: scale(0.75);
 }
 
-.button.isDisabled{
-color: rgba(0, 0, 0, 0.2);
-cursor: initial;
-}
-
-.button.isDisabled:active{
-    transform: none;
-}
-
 .level{
   color: white;
-}
-
-.albumCover{
-  width: 100%;
-  overflow: hidden;
-  padding: 20px;
-}
-
-.albumCover img{
-  width: 100%;
-  border-radius: 0.5em;
-  object-position: 50% 50%;
-  object-fit: cover;
-  z-index: 10;
 }
 
 .currentTimeContainer{
